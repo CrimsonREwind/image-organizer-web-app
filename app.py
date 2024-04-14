e1f14c96ad653ad95959a46436a42630999a84f2
Ayush Verma <106526797+CrimsonREwind@users.noreply.github.com>
2024-04-15 04:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import streamlit as st
-
+import clear
 import add
 import register
 import show
+import home
+import os
 
-home_tab, register_tab, add_tab, show_tab = st.tabs(["Home", "Register", "Add", "Show"])
 
-# with homeTab:
 
+home_tab, register_tab, add_tab, show_tab, clear_tab = st.tabs(["Home", "Register", "Add", "Show","Clear"])
+
+
+
+with home_tab:
+    home.home()
 with register_tab:
     register.register()
 with add_tab:
     add.add()
 with show_tab:
     show.show()
+with clear_tab:
+    clear.clear()
